25383bfc746f5a1696879989fd3e9b6180853d9e
Azir@9200 <aziruddin83@hotmail.com>
2024-11-14 01:47:52 +0000

    added initial task

--- index.js
+++ index.js
@@ -11,10 +11,8 @@ app.use(express.json());
 
 console.log("TASK user", process.env.DB_USER);
 
-const uri =
-  "mongodb+srv://task-master-server:IKcI4Xx4LY706ytb@cluster0.xq1u8gq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xq1u8gq.mongodb.net/task-master-server?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -25,16 +23,80 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const db = await client.db("taskmaster");
+    const tasksCollection = db.collection("tasks");
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
+    app.get("/", (req, res) => {
+      res.send("Task Master Server");
+    });
+
+    app.get("/tasks", async (req, res) => {
+      try {
+        const tasks = await tasksCollection.find({}).toArray();
+        res.json(tasks);
+      } catch (err) {
+        console.error("Error fetching tasks:", err);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
+
+    app.post("/tasks", async (req, res) => {
+      const newTask = req.body;
+      try {
+        const result = await tasksCollection.insertOne(newTask);
+        console.log("azir backend", result);
+        res.status(201).json(result);
+      } catch (err) {
+        console.error("Error creating task:", err);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
+    app.delete("/tasks/:id", async (req, res) => {
+      const taskId = req.params.id;
+
+      try {
+        const result = await tasksCollection.deleteOne({
+          _id: ObjectId(taskId),
+        });
+        if (result.deletedCount === 0) {
+          res.status(404).json({ error: "Task not found" });
+        } else {
+          res.json({ message: "Task deleted successfully" });
+        }
+      } catch (err) {
+        console.error("Error deleting task:", err);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
+    app.patch("/tasks/:_id", async (req, res) => {
+      const taskId = req.params.id;
+      const updatedTaskData = req.body;
+
+      try {
+        const result = await tasksCollection.updateOne(
+          { _id: new ObjectId(taskId) },
+          { $set: updatedTaskData }
+        );
+
+        if (result.matchedCount === 0) {
+          res.status(404).json({ error: "Task not found" });
+        } else {
+          res.json({ message: "Task updated successfully" });
+        }
+      } catch (err) {
+        console.error("Error updating task:", err);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    });
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
